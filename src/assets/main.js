1e5bd7f40d1e68aaa80639c28e93f880caee0f8a
mvaloris <mvaloris@gmail.com>
2018-04-23 18:48:03 -0400

    adds setHiddenFields function

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,13 +3,19 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+    if (answer = '' || attempt = '') {
+        setHiddenFields();
+    }
 }
 
 //implement new functions here
 
 function setHiddenFields() {
-    answer = Math.floor(Math.random()* 9999);
+    attempt = 0;
+    answer = Math.floor(Math.random()* 9999).toString();
+    while (answer.length < 4) {
+        answer = '0' + answer;
+    }
 }
 
 setHiddenFields();
